16cde3095daf5a3e79a9606146461c02ff867928
furtiveJack <tanguytraina@hotmail.fr>
2021-01-09 14:49:52 +0100

    Implement createTransaction/applyTransaction/getById

--- EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBean.java
+++ EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBean.java
@@ -1,5 +1,6 @@
 package com.ejbank.beans;
 
+import com.ejbank.model.AccountEntity;
 import com.ejbank.model.TransactionEntity;
 import com.ejbank.model.UserEntity;
 
@@ -7,6 +8,9 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Stateless
@@ -27,12 +31,11 @@ public class TransactionBean implements TransactionBeanLocal {
 
     @Override
     public List<TransactionEntity> getTransactionsByAccount(int accountId, int offset) {
-        List<TransactionEntity> transactions = em.createNamedQuery("TransactionEntity.getByAccount", TransactionEntity.class)
+        return em.createNamedQuery("TransactionEntity.getByAccount", TransactionEntity.class)
                 .setParameter("accountId", accountId)
                 .setFirstResult(offset)
                 .setMaxResults(10)
                 .getResultList();
-        return transactions;
     }
 
     @Override
@@ -41,5 +44,49 @@ public class TransactionBean implements TransactionBeanLocal {
                 .setParameter("accountId", accountId)
                 .getResultList()
                 .size();
+    }
+
+    @Override
+    public int createTransaction(AccountEntity srcAccount, AccountEntity dstAccount, double amount,
+                                  String comment, UserEntity author,
+                                  boolean applied) {
+        try {
+            TransactionEntity transaction = new TransactionEntity();
+            transaction.setAccount_id_from(srcAccount);
+            transaction.setAccount_id_to(dstAccount);
+            transaction.setAmount(amount);
+            transaction.setApplied(applied);
+            transaction.setComment(comment);
+            transaction.setAuthor(author);
+            transaction.setDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            em.persist(transaction);            // TODO: Deal with rollbacks
+            return transaction.getId();
+        } catch(Exception e) {
+            return -1;
+        }
+    }
+
+    @Override
+    public boolean applyTransaction(int id) {
+        try {
+            TransactionEntity transaction = em.find(TransactionEntity.class, id);
+            AccountEntity srcAccount = transaction.getAccount_id_from();
+            AccountEntity dstAccount = transaction.getAccount_id_to();
+            srcAccount.setBalance(srcAccount.getBalance() - transaction.getAmount());
+            dstAccount.setBalance(dstAccount.getBalance() + transaction.getAmount());
+            transaction.setApplied(true);
+            // TODO: Deal with rollbacks
+            em.persist(srcAccount);
+            em.persist(dstAccount);
+            em.persist(transaction);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public TransactionEntity getById(int id) {
+        return em.find(TransactionEntity.class, id);
     }
 }
--- EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBeanLocal.java
+++ EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBeanLocal.java
@@ -2,6 +2,7 @@ package com.ejbank.beans;
 
 import com.ejbank.model.AccountEntity;
 import com.ejbank.model.TransactionEntity;
+import com.ejbank.model.UserEntity;
 
 import javax.ejb.Local;
 import java.util.List;
@@ -14,4 +15,12 @@ public interface TransactionBeanLocal {
     List<TransactionEntity> getTransactionsByAccount(int accountId, int offset);
 
     int getNbTransactionsByAccount(int accountId);
+
+    int createTransaction(AccountEntity srcAccount, AccountEntity dstAccount, double amount,
+                           String comment, UserEntity author,
+                           boolean applied);
+
+    boolean applyTransaction(int id);
+
+    TransactionEntity getById(int id);
 }
